adc99b5632907d2947827f0fb8abdbdbfe7c6a36
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-10-08 19:50:33 +0900

    Add files via upload

--- Mockup_List/js/purpose.js
+++ Mockup_List/js/purpose.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {       
     $('.category-btn').on("click", function() {     //버튼 클릭시 호출되는 함수
-          $(this).addClass('active');      //클릭된 버튼에 속성(class 추가)
-        //   alert("[" + $(this).val() + "] 카테고리가 선택되었습니다.");   
-        //클릭된 버튼의 value 값  alert 띄우기
+        if($('.category-btn').hasClass("active")){
+            $(this).removeClass('active');      //클릭된 버튼에 속성(class 삭제)
+            alert("[" + $(this).val() + "] 카테고리 선택이 취소되었습니다."); 
+        } else{
+            $(this).addClass('active');      //클릭된 버튼에 속성(class 추가)
+            alert("[" + $(this).val() + "] 카테고리가 선택되었습니다."); // 확인 용도
+        }
     });
 });
