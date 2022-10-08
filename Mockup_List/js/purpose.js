003b781f0f3f1cf186c9cb0de501eca74a22e48c
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-10-08 19:39:36 +0900

    Add files via upload

---
+++ Mockup_List/js/purpose.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {       
+    $('.category-btn').on("click", function() {     //버튼 클릭시 호출되는 함수
+          $(this).addClass('active');      //클릭된 버튼에 속성(class 추가)
+    });
+});
+
