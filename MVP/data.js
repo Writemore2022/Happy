cad2a0d8c44ce45ee6eae7b1eb01f522e49ce0c7
JnSia <kia54682@gmail.com>
2022-11-28 20:05:53 +0900

    mvp-221128

---
+++ MVP/data.js
@@ -0,0 +1,5 @@
+const daegu = [
+  { name: '쓰기클럽, 저문', purpose: '등단', ganre: '시', mood: '따뜻한 공감' },
+  { name: '읽기클럽, 문저', purpose: '친목', ganre: '경제', mood: '따뜻한 붕어빵' },
+  { name: '딸기클럽, 저문', purpose: '딸기', ganre: '소설', mood: '차가운 냉면' },
+];
