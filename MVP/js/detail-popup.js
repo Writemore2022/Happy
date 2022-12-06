1e4a751ebba6cf541fa04dfa16639f75dd834331
purple-sunflower <jgh0711@naver.com>
2022-12-06 19:23:41 +0900

    jjang

---
+++ MVP/js/detail-popup.js
@@ -0,0 +1,7 @@
+const delBtn = document.getElementById("del_btn");
+const wrap = document.getElementById("wrap");
+
+delBtn.addEventListener('click', function() {
+    popup.style.display = "none";
+    alert("삭제")
+});
