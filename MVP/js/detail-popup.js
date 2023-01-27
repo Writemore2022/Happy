88dc8b15eceaaf9aa79e0e77aeaf51714f8e5692
JnSia <kia54682@gmail.com>
2023-01-27 14:48:56 +0900

    fix

--- MVP/js/detail-popup.js
+++
@@ -1,7 +0,0 @@
-const delBtn = document.getElementById("del_btn");
-const wrap = document.getElementById("wrap");
-
-delBtn.addEventListener('click', function() {
-    wrap.style.display = "none";
-    alert("삭제")
-});
