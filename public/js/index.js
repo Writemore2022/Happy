c1b4f4e578474333561f51b8a2523dc7811d37fd
JnSia <kia54682@gmail.com>
2023-04-04 16:14:55 +0900

    230404

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
+// webpack 사용을 위한 index.js
+
 import './main';
 import './category';
 import './detail';
-import './detailGroupInfo';
 import './sidemenu';
 import './firebase';
 import './calendar';
+import './feedback';
