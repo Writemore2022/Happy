9dabda86947d165c82b71ce75b05caf1a584bd5b
purple-sunflower <jgh0711@naver.com>
2023-05-15 20:05:57 +0900

    jjang

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,8 @@
+// Firebase 문서 (공식 문서 그대로 입니다.)
+// 그냥 다 가져와서 사용하는 것! (주석 따로 필요 없다고 판단)
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'; // Firebase 초기화를 위해 
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
@@ -9,7 +12,7 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig = { // Firebase 앱 객체
   apiKey: 'AIzaSyDEwoEKjZ--6SFCyLdlDaIsm967ZwxxwpM',
   authDomain: 'writemore-f6e18.firebaseapp.com',
   projectId: 'writemore-f6e18',
